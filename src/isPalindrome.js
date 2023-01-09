9e83f964ca3fedb53f19458cbaf7b131dceefc3c
JohaoChristianCGDev <jcfcg03@gmail.com>
2023-01-09 17:52:22 -0500

    primeroProblemas

--- src/isPalindrome.js
+++ src/isPalindrome.js
@@ -2,7 +2,13 @@
 // y debe retornar true si el numero es palindromo, de lo contrario debe retornar false
 
 const isPalindrome = function (x) {
-  // Escribe tu código aquí
+  const xComoString = Number(String(x).split('').reverse().join(''))
+
+  if (xComoString === x) {
+    return true
+  } else {
+    return false
+  }
 }
 
 export default isPalindrome
